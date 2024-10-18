0b9fa8e8f024628878db753d0d6bc8133535423d
bourgeois <juahsome2795@gmail.com>
2024-10-19 02:24:15 +0900

    feat: 구글맵 추가 및 현재 위치 설정

--- ios/UniRunFE/AppDelegate.mm
+++ ios/UniRunFE/AppDelegate.mm
@@ -1,11 +1,11 @@
 #import "AppDelegate.h"
-
 #import <React/RCTBundleURLProvider.h>
-
+#import <GoogleMaps/GoogleMaps.h>
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyAT_8tDdyRjfA6iYnpw8ASn6vpHJyaXXMg"];
   self.moduleName = @"UniRunFE";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
